42364e9ea8de3613ed53e9d4631608d18e55c18b
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 15:32:05 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validats :content, length: { minimum: 100 }
 end
